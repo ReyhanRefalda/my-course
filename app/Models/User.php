7457fe4e21ac38c238e5fa95fa8d779f8269b1ad
ReyhanRefalda <repaldaganss@gmail.com>
2024-12-02 21:58:42 +0700

    Persiapan dan Memulai Ngoding Sampai Nomor 6

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use Carbon\Carbon;
+
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +24,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'occupation',
+        'avatar',
     ];
 
     /**
@@ -44,5 +49,30 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'course_students');
+    }
+
+    public function subscribe_transactions()
+    {
+        return $this->hasMany(SubscribeTransaction::class);
+    }
+
+    public function hasActiveSubscription()
+    {
+        $latestSubscription = $this->subscribe_transactions()
+            ->where('is_paid', true)
+            ->latest('updated_at')
+            ->first();
+
+        if (!$latestSubscription) {
+            return false;
+        }
+
+        $subscriptionEndDate = Carbon::parse($latestSubscription->subscription_start_date)->addMonths(1);
+        return Carbon::now()->lessThanOrEqualTo($subscriptionEndDate); //true = dia berlangganan
     }
 }
